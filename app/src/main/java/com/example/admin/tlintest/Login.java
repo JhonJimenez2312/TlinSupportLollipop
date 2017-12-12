d3f2cb2f0d1dfe38bb0d71b2d23b9c6f41e43b2b
“Jhon” <Jota31337@hotmail.com>
2017-12-11 20:17:02 -0500

    correcion al llamado de ruta de imagen view

--- app/src/main/java/com/example/admin/tlintest/Login.java
+++ app/src/main/java/com/example/admin/tlintest/Login.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+
+
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
